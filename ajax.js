cff7b322cebc2e726e1da585dd04546d02ae3ac3
Brandon <brandonl271@gmail.com>
2019-04-10 11:51:20 -0700

    updated

---
+++ ajax.js
@@ -0,0 +1,14 @@
+function request(){
+    let httpRequest = new XMLHttpRequest();
+    
+    httpRequest.open("GET", "https://brandonlind10.github.io/learning-server/content.txt");
+    httpRequest.send();
+    
+    if(httpRequest.readyState === 4){
+        if(httpRequest.status === 200){
+            console.log(httpRequest.responseText);
+        }
+    }
+    
+    
+}
