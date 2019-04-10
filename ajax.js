005aa97bf4d3152d54ecbb3374e79b4d2882e7d0
Brandon <brandonl271@gmail.com>
2019-04-10 13:09:42 -0700

    updated

--- ajax.js
+++ ajax.js
@@ -12,7 +12,7 @@ function request(){
         if(httpRequest.readyState === 4){
             if(httpRequest.status === 200){
                 console.log(httpRequest.responseText);
-                messageEl.textContent=httpRequest.responsetext;
+                messageEl.textContent= httpRequest.responseText;
             }
         }
         console.log("ran");
